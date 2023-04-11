9fa9b899cb86e124be538d5e6c5bf3b5ae3946d9
Martell <vovasamb@yandex.ru>
2023-04-11 13:40:04 +0300

    Criteria API

--- src/main/java/pakeyg/repository/BookRepository.java
+++ src/main/java/pakeyg/repository/BookRepository.java
@@ -8,5 +8,12 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findAllByAuthorId(Integer authorId);
+//    CriteriaBuilder builder = session.getCriteriaBuilder();
+//    CriteriaQuery<Book> bookCriteriaQuery = builder.createQuery(Book.class);
+//    Root<Book> root = bookCriteriaQuery.from(Book.class);
+//    bookCriteriaQuery.select(root).where(builder.like(root.get("author_id"), authorId.toString()));
+//    Query<Dog> query = session.createQuery(bookCriteriaQuery);
+//    return query.getResultList();
+
     List<Book> findAllByName(String name);
 }
